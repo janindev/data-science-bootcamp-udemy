099f2c02b0ee85277cb51eac20bf63fd70ef1afb
jwillems-snv <82822930+jwillems-snv@users.noreply.github.com>
2022-02-28 09:58:09 +0100

    wrote v2

--- calc_practice_w_class.py
+++ calc_practice_w_class.py
@@ -8,14 +8,15 @@ class MulCalc:
     a = x * 6
     b = x * 10
 
+
     def __init__(self,a,b):
         self.a = a
         self.b = b
 
-        def calc1(x):
+        def calc1(self):
             return a
 
-        def calc2(x):
+        def calc2(self):
             return b
 
 
@@ -27,3 +28,24 @@ def Calc():
 
 Calc()
 
+###################  v.2.  ###############################
+
+# Define Class MulCalc with Methods
+class MulCalc:
+
+    def __init__(self, x=0):
+        self.x = x
+
+    def calc1(self):
+        return x * 6
+
+    def calc2(self):
+        return x * 10
+
+# Define Function Calc to provide output from MulCalc Class Methods at once
+def Calc():
+    print("The value of calc1 = " + str(MulCalc().calc1()))
+    print("The value of calc2 = " + str(MulCalc().calc2()))
+
+# Run Calc 
+Calc()
